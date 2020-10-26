84b5cd95617bd1633f6546fc0069a6d9d828c041
nitinchouhan1010 <nitinchouhan4444@gmail.com>
2020-10-26 15:42:25 +0530

    2 files modified

--- src/TeamDetail.js
+++ src/TeamDetail.js
@@ -20,32 +20,32 @@ const TeamDetail = () => {
         </div>
         <h6 className="d-flex align-items-center">
           Todays match is between :{" "}
-          <span className="p-2 h5 mb-0">{Team.TeamFirst}</span> and
-          <span className="p-2 h5 mb-0"> {Team.TeamSecond}</span>
+          <span className="p-2 h5 mb-0">{Team && Team.TeamFirst}</span> and
+          <span className="p-2 h5 mb-0"> {Team && Team.TeamSecond}</span>
         </h6>
         <div className="card text-center mb-4">
           <div className="card-body">
             <h6>Captain of the Team</h6>
-            <h3>{Team.captain}</h3>
+            <h3>{Team && Team.captain}</h3>
           </div>
         </div>
         <div className="card text-center mb-4">
           <div className="card-body">
             <h6>Vice Captain of the Team</h6>
-            <h3>{Team.vcaptain}</h3>
+            <h3>{Team && Team.vcaptain}</h3>
           </div>
         </div>
         <h6>Other Team Player will be</h6>
         <ul className="list-inline">
-          <li>{Team.player3}</li>
-          <li>{Team.player4}</li>
-          <li>{Team.player5}</li>
-          <li>{Team.player6}</li>
-          <li>{Team.player7}</li>
-          <li>{Team.player8}</li>
-          <li>{Team.player9}</li>
-          <li>{Team.player10}</li>
-          <li>{Team.player11}</li>
+          <li>{Team && Team.player3}</li>
+          <li>{Team && Team.player4}</li>
+          <li>{Team && Team.player5}</li>
+          <li>{Team && Team.player6}</li>
+          <li>{Team && Team.player7}</li>
+          <li>{Team && Team.player8}</li>
+          <li>{Team && Team.player9}</li>
+          <li>{Team && Team.player10}</li>
+          <li>{Team && Team.player11}</li>
         </ul>
       </div>
     </div>
